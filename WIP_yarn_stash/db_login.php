ee7a00c517d995746464442b4237d0226c7192d6
Hazel Windle <hwindle83@googlemail.com>
2022-03-05 22:06:30 +0000

    Created delete.html file, took old password out of db_login.php script

--- WIP_yarn_stash/db_login.php
+++ WIP_yarn_stash/db_login.php
@@ -1,6 +1,6 @@
 <?php
 
   $database_username = 'hazel';
-  $database_password = '$%294tr7usdJ';
+  $database_password = '#'; #contained elsewhere
   $pdo_conn = new PDO('mysql:host=localhost;dbname=yarn_haven', $database_username, $database_password);
   $pdo_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
