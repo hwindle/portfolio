2b990412600df64c78f283ab8240c5d064596dab
Hazel Windle <hwindle83@googlemail.com>
2022-03-07 19:36:20 +0000

    Created footer.php template

---
+++ WIP_yarn_stash/footer.php
@@ -0,0 +1,17 @@
+<div class="errors">
+            <p>Database entry added or pdo error on line 10</p>
+        </div>
+    </section>
+    <footer>
+        <p>Made by me, &copy; Hazel Windle 2018 </p>
+    </footer>
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
