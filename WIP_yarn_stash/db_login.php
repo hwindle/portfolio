2b48ac660187420f51f2b2db0b32d45829499310
Hazel Windle <hwindle83@googlemail.com>
2022-03-07 19:38:29 +0000

    Took out old PHP files and outdated JavaScript node modules

--- WIP_yarn_stash/db_login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-  $database_username = 'hazel';
-  $database_password = '#'; #contained elsewhere
-  $pdo_conn = new PDO('mysql:host=localhost;dbname=yarn_haven', $database_username, $database_password);
-  $pdo_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
