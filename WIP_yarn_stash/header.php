df335fb3d26862f92e03c52f85a59f13010f0023
Hazel Windle <hwindle83@googlemail.com>
2022-03-23 14:32:53 +0000

    Added update and delete buttons to the table

--- WIP_yarn_stash/header.php
+++ WIP_yarn_stash/header.php
@@ -8,6 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
         <link rel="stylesheet" href="css/foundation.min.css">
+        <link rel="stylesheet" href="css/foundation-icons/foundation-icons.css">
         <link rel="stylesheet" href="css/main.css">
     </head>
     <body class="wrapper">
