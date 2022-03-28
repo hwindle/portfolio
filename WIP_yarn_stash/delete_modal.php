4dcbdf2fc02d5f0b90863d1244e628b69ed817d3
Hazel Windle <hwindle83@googlemail.com>
2022-03-28 09:43:45 +0100

    Left the buggy delete modal in

--- WIP_yarn_stash/delete_modal.php
+++ WIP_yarn_stash/delete_modal.php
@@ -1,19 +1,18 @@
 <button class="delete" data-open="delete-modal">X</button></p>
 
+<!-- DELETE MODAL -->
 <div class="reveal" id="delete-modal" aria-labelledby="delete-modal-header" data-reveal>
-    <h1 id="delete-modal-header">Deleting Yarn</h1>
-    <p class="lead">Are you sure you wish to delete 
-        <span class="del-yarn">Brand Yarn name, weight</span>?</p>
-    <p class="delete-action-btns">
-        <button class="no" data-close aria-label="Close Delete Modal"   type="button">
-            <span aria-hidden="true">&times;</span>
-            No
-        </button>
-        <a href="#">
-        <button class="yes" id="delete-yes">
-            <span class="fi-tick"></span>
-            Yes
-        </button>
-        </a>
-    </p>
-</div>
+            <h1 id="delete-modal-header">Deleting Yarn</h1>
+            <p class="lead">Are you sure you wish to delete 
+                <span class="del-yarn">Brand Yarn name, weight</span>?</p>
+            <p class="delete-action-btns">
+                <button class="no" aria-label="Close Delete Modal"   type="button"> 
+                    <span aria-hidden="true">&times;</span>
+                    No
+                </button>
+                <button class="yes" id="delete-yes" type="button">
+                    <span class="fi-tick"></span>
+                    Yes
+                </button>
+            </p>
+        </div>
