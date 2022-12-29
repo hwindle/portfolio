8d19cad1a035d5cf65d4d33e09a4ec39791f1ae0
Hazel Windle <hwindle83@googlemail.com>
2022-12-29 10:29:58 +0000

    Finished styling the contact form

--- JS/ui.js
+++ JS/ui.js
@@ -1,11 +1,19 @@
 // get the menu mobile button
 const mobileBtn = document.querySelector('#mobile-menu-link');
-
+// the element with the arrow chevron in it
+const arrowSpan = document.querySelector('li.phone-menu-btn > span');
 // get the ul.main-menu li.menu-items for adding the class .show to.
 const liArray = document.querySelectorAll('ul.main-menu > li.menu-item');
 
 mobileBtn.addEventListener('mousedown', () => {
   liArray.forEach((item) => {
     item.classList.toggle('show');
+    if (item.classList.contains('show')) {
+      arrowSpan.classList.remove('fa-chevron-down');
+      arrowSpan.classList.add('fa-chevron-up');
+    } else {
+      arrowSpan.classList.remove('fa-chevron-up');
+      arrowSpan.classList.add('fa-chevron-down');
+    }
   });
 });
